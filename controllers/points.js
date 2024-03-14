6e15bfb4e690caa813508714aa332841d16d4a4f
Amirhossein Sefati <ahsefati77@gmail.com>
2024-03-13 19:37:18 -0600

    Completing full details for Within a Box api. Making it more optimized and scalable.

--- controllers/points.js
+++ controllers/points.js
@@ -1,11 +1,25 @@
 import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const { db, pgp } = require('../pgdb.cjs');
+const require = createRequire(import.meta.url)
+const { db, pgp } = require('../pgdb.cjs')
 
+
+export const getAllSources = async (req, res) => {
+    try {
+        const query_str = `
+            SELECT
+            *
+            FROM
+            sources;
+        `
+        const sources = await db.any(query_str)
+        res.status(200).send(sources)
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 export const getPointsInABox = async (req, res) => {
     try {
-        console.log(req)
         const { min_lat, min_lon, max_lat, max_lon } = req.body
         const query_str = `
             SELECT
@@ -19,7 +33,54 @@ export const getPointsInABox = async (req, res) => {
             WHERE
             ST_Within(geom, ST_MakeEnvelope(${min_lat}, ${min_lon}, ${max_lat}, ${max_lon}, 4326));
         `
-        const points = await db.any(query_str);
+        const points = await db.any(query_str)
+        res.status(200).send(points)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export const getPointsInABoxWithFilters = async (req, res) => {
+    try {
+        const { min_lat, min_lon, max_lat, max_lon, username, sourceId } = req.body
+        
+        let usernameSelector;
+        if (username!==undefined){
+            usernameSelector = `AND u."username" = '${username}'`
+        }else{
+            usernameSelector = ``
+        }
+
+        let sourceSelector;
+        if (sourceId!==undefined){
+            sourceSelector = `AND u."sourceId" = '${sourceId}'`
+        }else{
+            sourceSelector = ``
+        }
+
+        const query_str = `
+            SELECT
+            p.pointid,
+            u."username",
+            ST_X(p.geom::geometry) AS longitude,
+            ST_Y(p.geom::geometry) AS latitude,
+            to_timestamp(p.timestamp) AT TIME ZONE 'UTC' AS datetime,
+            u."sourceId",
+            p.metadata
+            FROM
+                points p
+            JOIN
+                users u ON p.userId = u."userId"
+            JOIN
+                sources s ON u."sourceId" = s."sourceId"
+            WHERE
+                ST_Within(p.geom, ST_MakeEnvelope(${min_lat}, ${min_lon}, ${max_lat}, ${max_lon}, 4326))
+                ${sourceSelector}
+                ${usernameSelector}
+            ;
+        `
+        const points = await db.any(query_str)
 
         res.status(200).send(points)
     } catch (error) {
--- routes/points.js
+++ routes/points.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import {getPointsInABox} from '../controllers/points.js'
+import {getAllSources, getPointsInABox, getPointsInABoxWithFilters} from '../controllers/points.js'
 
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.post('/inabox', getPointsInABox)
+router.post('/inaboxwithfilter', getPointsInABoxWithFilters)
+router.get('/allsources', getAllSources)
 
 export default router
